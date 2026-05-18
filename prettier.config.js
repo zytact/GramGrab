2f3fc897b9d56e68f077ed0e6534a7a03f6cd6a5
Arnab Chakraborty <that.zytact@gmail.com>
2026-05-18 20:07:17 +0530

    switch to js for configs to make them work on ci

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,5 @@
-import type { Config } from 'prettier';
-
-const config: Config = {
+/** @type {import('prettier').Config} */
+const config = {
   singleQuote: true,
   trailingComma: 'es5',
   tabWidth: 2,
